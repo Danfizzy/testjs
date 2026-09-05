d0ad6b2e03ca978a4986a1e039e7cd7ef4dbf336
Danfizzy <danielohamazue@gmail.com>
2026-09-05 06:51:46 -0700

    added index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+// Task 1
+// let fullName = prompt('What is your name?');
+// let age = prompt('How old are you?');
+// let school = prompt('Which school do you attend?');
+// let department = prompt('What is your department?')
+
+// console.log(fullName);
+// console.log(age);
+// console.log(school);
+// console.log(department);
+
+
+// Task 3
+// let productName = prompt('Product Name:');
+// let productPrice = prompt('Product Price:');
+// let quantity = prompt('Product Quantity:');
+
+// let totalCost = productPrice * quantity;
+
+// console.log(`${quantity} ${productName} will cost ${totalCost}`);
+// alert(`${quantity} ${productName} will cost ${totalCost}`);
+
+
+// Task 4
+// let fullName = prompt('What is your name?');
+// let age = prompt('How old are you?');
+// let futureYear = prompt('Insert Future Year');
+
+// let determineNumberOfYears = futureYear - 2026;
+
+// let futureAge = parseInt(age) + determineNumberOfYears;
+
+// console.log(`${fullName}, you will be ${futureAge} years old in ${futureYear}`);
+
+// Task 5
+let employee = prompt('What is your name?');
+let monthlySalary = prompt('What is your monthly income?');
+let monthsWorked = prompt('How many months have you worked for?');
+
+let totalEarnings = monthlySalary * monthsWorked;
+
+console.log(`${employee}, your total earnings for ${monthsWorked} months is ${totalEarnings}`);
